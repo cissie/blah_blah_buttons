5ac521b0c88180956a38e429d455d0731f41d37f
cissie <cissie@gmail.com>
2015-02-09 16:10:45 -0800

    Added django-shop package

--- blah_blah/urls.py
+++ blah_blah/urls.py
@@ -1,8 +1,22 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from blahblahbuttons import views
+from shop import urls as shop_urls
+admin.autodiscover()
 
-urlpatterns = patterns(
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blahblahbuttons/', include('blahblahbuttons.urls')),
+    url(r'^shop/', include('shop.urls')),
 
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
--- blahblahbuttons/models.py
+++ blahblahbuttons/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from shop.models.productmodel import Product
+from django.db import models
 
-# Create your models here.
+class Buttons(Product):
+    size = models.CharField(max_length=255)
+    description = models.TextField()
+    amount = models.IntegerField()
+    photo = models.ImageField(upload_to='product_photo',
+                            blank=True)
+
+    class Meta: pass
